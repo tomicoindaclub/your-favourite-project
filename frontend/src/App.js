872be82ed8f8a7da6527fedfb354b1edb6fd6388
Petruska Tomi <tamas.petruska99@gmail.com>
2022-12-05 12:15:50 +0100

    input

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useEffect, useState } from "react";
+import Main from "./components/Main";
 
 function App() {
+  const [inputCity, setInputCity] = useState("");
+  const [cityName, setCityName] = useState("");
+
+  const handleClick = () => {
+    let cityName = inputCity;
+    setCityName(cityName);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <input
+          type="text"
+          name="filter"
+          id="filter"
+          value={inputCity}
+          onChange={(e) => setInputCity(e.target.value)}
+        />
+        <button onClick={handleClick}>KERESS</button>
+        <Main cityName={cityName} />
+      </div>
+    </>
   );
 }
 
---
+++ frontend/src/components/Main.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Main({ cityName }) {
+  return <div>{cityName}</div>;
+}
+
+export default Main;
