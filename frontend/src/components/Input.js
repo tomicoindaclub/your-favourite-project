47f4edee017819acac4d493b7b26672d42951806
joobadam <joobadam@gmail.com>
2022-12-06 17:54:34 +0000

    autocomplete próba

--- frontend/src/components/Input.js
+++ frontend/src/components/Input.js
@@ -5,18 +5,25 @@ function Input() {
 
     const [search, setSearch] =useState("")
     const [currentData, setCurrentData] = useState([])
-    const [img, setImg] = useState();
+    const [autoComp, setAutoComp] = useState([]);
 
 
 
-    const apiKey= `0c183950ab4549da8fb115413220512`
+    const apiKey = `0c183950ab4549da8fb115413220512`
     const fetchUrl = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${search}&aqi=no`
 
+    const autoComplete = `https://api.weatherapi.com/v1/search.json?key=${apiKey}&q=${search}&aqi=no`
     
     
 
     const handleChange =  (e) =>{
         setSearch(e.target.value)
+
+
+
+        fetch(autoComplete)
+        .then(res =>  res.json())
+        .then(data =>  setAutoComp(data))
     }
 
     const handleClick =  () =>{
@@ -24,14 +31,11 @@ function Input() {
             fetch(fetchUrl)
                     .then(res =>  res.json())
                     .then(data =>  setCurrentData(data))
-        
-    
-
                    
     } 
 
     console.log(currentData)
-
+    console.log(autoComp)
   return (
     
     <div>
@@ -39,6 +43,12 @@ function Input() {
             <input type="text" name="seacrh" onChange={handleChange} value={search} />
 
             <button onClick={handleClick}>seacrh</button>
+
+
+            {autoComp[0] ? <option>{autoComp[0].name}</option> : null}
+            {autoComp[1] ? <option>{autoComp[1].name}</option> : null}
+            {autoComp[2] ? <option>{autoComp[2].name}</option> : null}
+
 
             {currentData.location ? <h1>{currentData.location.name}</h1> : null}
             {currentData.location ? <h2>{currentData.location.country}</h2> : null}
