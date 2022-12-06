4f6bd29f139628117fba7b70f2273aa5da046b2a
Petruska Tomi <tamas.petruska99@gmail.com>
2022-12-06 09:09:41 +0100

    img fetch try

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,13 +5,17 @@ import Header from "./components/Header";
 
 function App() {
   const apiKey = "0c183950ab4549da8fb115413220512";
+  // const imageApiKey =
+  //   "563492ad6f917000010000019fe6895bc4de4b2f816d02c64408d69f";
 
   const [searchInput, setSearchInput] = useState("");
   const [weatherhData, setWeatherData] = useState([]);
+  // const [imageData, setImageData] = useState([]);
 
   const getSearchInput = (input) => {
     let searchInput = input;
     setSearchInput(searchInput);
+    console.log(searchInput);
   };
 
   useEffect(() => {
@@ -23,9 +27,25 @@ function App() {
       })
       .then((data) => {
         setWeatherData(data);
-        console.log(searchInput);
         console.log(weatherhData);
       });
+
+    // fetch(`https://api.pexels.com/v1/search?query=${searchInput}&per_page=1`, {
+    //   method: "GET",
+    //   headers: {
+    //     Accept: "application/json",
+    //     Authorization: `${imageApiKey}`,
+    //   },
+    // })
+    //   .then((response) => {
+    //     return response.json();
+    //   })
+    //   .then((data) => {
+    //     setImageData(data);
+    //     let imgURL = imageData.photos[0].src.original;
+    //     console.log(imageData)
+    //     console.log(imgURL);
+    //   });
   }, [searchInput]);
 
   return (
--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -1,7 +1,14 @@
 import React from "react";
 
 function Main({ weatherData }) {
-  return <div>{weatherData}</div>;
+  return (
+    <>
+      <div>
+        <p>{weatherData.current.temp_c}</p>
+      </div>
+      {/* <div>{imageData}</div> */}
+    </>
+  );
 }
 
 export default Main;
